3e72a034ce5cece429defbca23f8b93fdaf2766f
Dieter Niklaus <dieter.niklaus@gmx.net>
2015-07-21 13:35:25 +0200

    Integrated Adafruit MCP9808 Temperature Sensor
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- prepare-workspace.bat
+++ prepare-workspace.bat
@@ -1,18 +1,33 @@
 @echo off
 ::-----------------------------------------------------------------------------
+:: Paths
+::-----------------------------------------------------------------------------
+set SCRIPT_DIR=%~dp0%
+set ProjectHome=%SCRIPT_DIR:~0,-1%
+set WorkspaceDir=%ProjectHome%\workspace
+set ArduinoTools=%ProjectHome%\..\Tools
+set ThisProjTools=%ProjectHome%\tools
+
+::-----------------------------------------------------------------------------
+:: Load Script Environment / Configuration
+::-----------------------------------------------------------------------------
+for /f "delims=" %%x in (%ProjectHome%\env.config) do (set "%%x")
+
+
+::-----------------------------------------------------------------------------
 :: Configured Settings
 ::-----------------------------------------------------------------------------
 :: Arduino IDE (see http://arduino.cc/en/Main/OldSoftwareReleases) 
-set ArduinoDownloadUrl=http://arduino.cc/download.php?f=
-set ArduinoVer=1.5.6-r2
+::set ArduinoDownloadUrl=http://arduino.cc/download.php?f=
+::set ArduinoVer=1.5.6-r2 ::to be set in env.config
 
 :: Eclipse Arduino Workbench Bundle (see http://www.baeyens.it/eclipse/download.php)
-set EclipseArduinoDownloadUrl=http://www.baeyens.it/eclipse/download/product
-set EclipseArduinoVer=2014-12-28_02-06-00
+::set EclipseArduinoDownloadUrl=http://www.baeyens.it/eclipse/download/product
+::set EclipseArduinoVer=2015-01-13_02-06-58 ::to be set in env.config
 
 :: Expected Project Location (Eclipse CDT cannot deal with relative paths)
-set ArduinoProjects=C:\git\arduino-projects
-set ExpectedProjectHome=%ArduinoProjects%\arduino-lcdkeypad-test\
+:: set ArduinoProjects=C:\git\arduino-projects ::to be set in env.config
+set ExpectedProjectHome=%ArduinoProjects%\%ProjectSubDir%
 
 ::-----------------------------------------------------------------------------
 :: Get the OS Variant
@@ -22,15 +37,6 @@ IF "%PROCESSOR_ARCHITECTURE%;%PROCESSOR_ARCHITEW6432%"=="x86;" (
 ) ELSE (
   set OsVariant=win64
 )
-
-::-----------------------------------------------------------------------------
-:: Paths
-::-----------------------------------------------------------------------------
-set SCRIPT_DIR=%~dp0%
-set ProjectHome=%SCRIPT_DIR%
-set WorkspaceDir=%ProjectHome%\workspace
-set ArduinoTools=%ProjectHome%\..\Tools
-set ThisProjTools=%ProjectHome%\tools
 
 ::-----------------------------------------------------------------------------
 :: Assert correct path
@@ -57,9 +63,6 @@ set CurEclipseArduino=%EclipseArduinoRevs%\%OsVariant%.%EclipseArduinoVer%\eclip
 
 :: 7Zip
 set Archiver=%ThisProjTools%\7za920\7za.exe
-
-:: curl (unused)
-set Curl=%ThisProjTools%\curl\curl.exe
 
 :: wget
 set Wget=%ThisProjTools%\wget\wget.exe
@@ -92,7 +95,6 @@ if not exist "%CurArduino%" (
     md "%ArduinoRevs%"
   )
   if not exist "%ArduinoRevs%\arduino-%ArduinoVer%-windows.zip" (
-    ::%Curl% -# -o "%ArduinoRevs%\arduino-%ArduinoVer%-windows.zip" "%ArduinoDownloadUrl%/arduino-%ArduinoVer%-windows.zip"
     %Wget% --tries=0 --output-document="%ArduinoRevs%\arduino-%ArduinoVer%-windows.zip" "%ArduinoDownloadUrl%/arduino-%ArduinoVer%-windows.zip"
   )
   %Archiver% x -y -o%ArduinoRevs% %ArduinoRevs%\arduino-%ArduinoVer%-windows.zip
@@ -110,7 +112,6 @@ if not exist "%CurEclipseArduino%" (
     md "%EclipseArduinoRevs%"
   )
   if not exist "%EclipseArduinoRevs%\%OsVariant%.%EclipseArduinoVer%.tar.gz" (
-    ::%Curl% -# -o "%EclipseArduinoRevs%\%OsVariant%.%EclipseArduinoVer%.tar.gz" "%EclipseArduinoDownloadUrl%/%OsVariant%.%EclipseArduinoVer%.tar.gz"
     %Wget% --tries=0 --output-document="%EclipseArduinoRevs%\%OsVariant%.%EclipseArduinoVer%.tar.gz" "%EclipseArduinoDownloadUrl%/%OsVariant%.%EclipseArduinoVer%.tar.gz"
   )
   %Archiver% x -y -o%EclipseArduinoRevs% %EclipseArduinoRevs%\%OsVariant%.%EclipseArduinoVer%.tar.gz
@@ -135,7 +136,7 @@ if not exist "%WorkspaceDir%\.metadata" (
 :: run first build, would fail
 call build.bat
 if %errorlevel% == 0 goto end
-:: revert src/.project that has been made dirty by the failing build
+:: revert src/.project that have been made dirty by the failing build
 %Git% checkout -- %ProjectHome%\src\.project
 
 :: run second build
--- src/arduino_lcdkeypad_test.cpp
+++ src/arduino_lcdkeypad_test.cpp
@@ -7,7 +7,11 @@
 // LcdKeypad, https://github.com/dniklaus/arduino-display-lcdkeypad
 #include <LcdKeypad.h>
 
+#include "Adafruit_MCP9808.h"
+// Create the MCP9808 temperature sensor object
+Adafruit_MCP9808* tempsensor = 0;
 LcdKeypad* myLcdKeypad = 0;
+bool isSelectPressed = false;
 
 class MyLcdKeypadAdapter : public LcdKeypadAdapter
 {
@@ -22,41 +26,57 @@ public:
 
   void handleKeyChanged(LcdKeypad::Key newKey)
   {
-    Serial.print("newKey: ");
-    Serial.println(newKey);
+    Serial.print("MyLcdKeypadAdapter::handleKeyChanged(), newKey: ");
+    Serial.println((LcdKeypad::NO_KEY == newKey)     ? "NO_KEY"     :
+                   (LcdKeypad::SELECT_KEY == newKey) ? "SELECT_KEY" :
+                   (LcdKeypad::LEFT_KEY == newKey)   ? "LEFT_KEY"   :
+                   (LcdKeypad::UP_KEY == newKey)     ? "UP_KEY"     :
+                   (LcdKeypad::DOWN_KEY == newKey)   ? "DOWN_KEY"   :
+                   (LcdKeypad::RIGHT_KEY == newKey)  ? "RIGHT_KEY"  : "OOPS!! Invalid value!!");
 
-    if (LcdKeypad::UP_KEY == newKey)
-    {
-      m_value++;
-    }
-    else if (LcdKeypad::DOWN_KEY == newKey)
-    {
-      m_value--;
-    }
     if (0 != m_lcdKeypad)
     {
-      m_lcdKeypad->setCursor(0, 0);
-      m_lcdKeypad->print("Zahl:       ");
-      if (m_value < 100)
+      if (!isSelectPressed)
       {
-        m_lcdKeypad->print(" ");
-      }
-      if (m_value < 10)
-      {
-        m_lcdKeypad->print(" ");
-      }
-      m_lcdKeypad->print(m_value);
-
-      m_lcdKeypad->setCursor(0, 1);
-      m_lcdKeypad->print("Bin:  ");
-
-      for (int i = 7; i >= 0; i--)
-      {
-        m_lcdKeypad->print(((m_value >> i) & 1) ? "1" : "0");
-        if (i == 4)
+        if (LcdKeypad::SELECT_KEY == newKey)
         {
-          m_lcdKeypad->print(" ");
+          isSelectPressed = true;
         }
+      }
+      else
+      {
+        if (LcdKeypad::UP_KEY == newKey)
+        {
+          m_value++;
+        }
+        else if (LcdKeypad::DOWN_KEY == newKey)
+        {
+          m_value--;
+        }
+
+//        m_lcdKeypad->setCursor(0, 0);
+//        m_lcdKeypad->print("Temp [°C]:  ");
+//        if (m_value < 100)
+//        {
+//          m_lcdKeypad->print(" ");
+//        }
+//        if (m_value < 10)
+//        {
+//          m_lcdKeypad->print(" ");
+//        }
+//        m_lcdKeypad->print(m_value);
+//
+//        m_lcdKeypad->setCursor(0, 1);
+//        m_lcdKeypad->print("Bin:  ");
+//
+//        for (int i = 7; i >= 0; i--)
+//        {
+//          m_lcdKeypad->print(((m_value >> i) & 1) ? "1" : "0");
+//          if (i == 4)
+//          {
+//            m_lcdKeypad->print(" ");
+//          }
+//        }
       }
     }
   }
@@ -66,13 +86,40 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("Hello from Arduino LCD Keypad Shield test!");
+
+  // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x19) for example
+  tempsensor = new Adafruit_MCP9808();
+  if (!tempsensor->begin()) {
+    Serial.println("Couldn't find MCP9808!");
+    while (1);
+  }
+
   myLcdKeypad = new LcdKeypad();
   myLcdKeypad->attachAdapter(new MyLcdKeypadAdapter(myLcdKeypad));
   myLcdKeypad->setBackLightOn(true);
+
+  myLcdKeypad->setCursor(0, 0);
+//  myLcdKeypad->print("LCD Keypad Test");
+//
+//  myLcdKeypad->setCursor(0, 1);
+//  myLcdKeypad->print("Press SELECT!");
 }
 
 void loop()
 {
-  scheduleTimers();
+  // Read and print out the temperature, then convert to *F
+  float c = tempsensor->readTempC();
+  float fc = c * 9.0 / 5.0 + 32;
+  float f = tempsensor->readTempF();
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t");
+  Serial.print(f); Serial.print("*F \t");
+  Serial.print(fc); Serial.println("*F (calc)");
+
+  myLcdKeypad->setCursor(0, 0);
+  myLcdKeypad->print("Temp. [*C]: ");
+  myLcdKeypad->print(c);
+
+  delay(1000);
 }
 
